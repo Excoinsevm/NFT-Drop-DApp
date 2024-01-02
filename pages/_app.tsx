3542d83d954260fc9ae7389b46d6768c597eaf3d
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-01-02 05:47:40 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Bitrock } from "@thirdweb-dev/chains";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 
