e00c155825992ec276c25c39820cda4e75534687
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-01-02 05:41:22 +0000

    Update [id].tsx

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -138,11 +138,11 @@ function NFTDropPage({ collection }: Props) {
                 <header className='flex items-center justify-between'>
                     <Link href={'/'}>
                         <h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>
-                            The{' '}
+                            NFT{' '}
                             <span className='font-extrabold underline decoration-pink-600/50'>
-                                NFT
+                                Drop's
                             </span>{' '}
-                            Market Place
+                            on Bitrock
                         </h1>
                     </Link>
                     <button onClick={() => (address ? disconnect() : connectWithMetamask())} className='rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base'>
